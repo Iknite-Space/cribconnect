6adfdc0fe79516ef5560ce3ad176d1a8249bef9c
Ryan J. Yoder <ryan@iknite.studio>
2025-06-06 22:07:45 +0100

    Remove files from submodule

--- infra/cloudformation/aws-cloudformation-templates/custom-resource-lambdas/src/pg-database/package.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-# this is just a temporary script while debugging
-
-GOARCH=amd64 GOOS=linux go build -tags lambda.norpc -o bootstrap ./...
-
-rm ../../zip/pgDatabase.zip
-zip ../../zip/pgDatabase.zip bootstrap
-
