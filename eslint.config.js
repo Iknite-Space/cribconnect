356e889e0e515dcd515400323aba623168bd2d82
AMPAM-Estella <estellaabo@gmail.com>
2025-06-11 13:04:19 +0100

    Adjusted the config on eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,39 @@
 // eslint.config.js
 import js from '@eslint/js';
 import react from 'eslint-plugin-react';
+import globals from 'globals';
 
 export default [
   js.configs.recommended,
   {
-    files: ['**/*.jsx', '**/*.tsx'],
-    plugins: { react },
+    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+    plugins: {
+      react
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true
+        }
+      },
+      globals: {
+        ...globals.browser,    // for things like `document`
+        ...globals.es2021
+      }
+    },
     rules: {
       'react/react-in-jsx-scope': 'off',
-    },
+      'no-unused-vars': 'warn' // optional: reduce from error to warning
+    }
   },
+  {
+    files: ['**/*.test.js', '**/*.spec.js'],
+    languageOptions: {
+      globals: {
+        ...globals.jest
+      }
+    }
+  }
 ];
