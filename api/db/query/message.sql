8c361029752038e6ba42cab54d199e0f9cfef245
Speedy <bomaflorent23@gmail.com>
2025-07-05 14:12:55 +0100

    Added sqlc statemnts and generated codes for user registration, login and complete profile

--- api/db/query/message.sql
+++ api/db/query/message.sql
@@ -1,17 +1,36 @@
--- name: CreateMessage :one
-INSERT INTO message (thread, sender, content)
-VALUES ($1, $2, $3)
+-- -- name: CreateMessage :one
+-- INSERT INTO message (thread, sender, content)
+-- VALUES ($1, $2, $3)
+-- RETURNING *;
+
+-- -- name: GetMessageByID :one
+-- SELECT * FROM message
+-- WHERE id = $1;
+
+-- -- name: GetMessagesByThread :many
+-- SELECT * FROM message
+-- WHERE thread = $1
+-- ORDER BY created_at DESC;
+
+-- -- name: DeleteMessage :exec
+-- DELETE FROM message
+-- WHERE id = $1;
+
+
+-- name: RegisterUser :one
+INSERT INTO users (user_id,email)
+VALUES ($1, $2)
+RETURNING user_id,email;
+
+-- name: UpdateUserProfile :one
+UPDATE users
+SET
+    fname = COALESCE($2, fname),
+    lname = COALESCE($3, lname),
+    phoneno = COALESCE($4, phoneno),
+    birthdate = COALESCE($5, birthdate),
+    bio = COALESCE($6, bio),
+    preferences = COALESCE($7, preferences),
+    profile_picture = COALESCE($8, profile_picture)
+WHERE user_id = $1
 RETURNING *;
-
--- name: GetMessageByID :one
-SELECT * FROM message
-WHERE id = $1;
-
--- name: GetMessagesByThread :many
-SELECT * FROM message
-WHERE thread = $1
-ORDER BY created_at DESC;
-
--- name: DeleteMessage :exec
-DELETE FROM message
-WHERE id = $1;
