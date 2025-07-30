ab6db330062f40997f127480b5ce1ac4cf997058
Speedy <bomaflorent23@gmail.com>
2025-07-30 11:45:29 +0100

    Added Matching handler and route to calculate match score

--- api/api/routes.go
+++ api/api/routes.go
@@ -33,6 +33,7 @@ func (h *UserHandler) WireHttpHandler() http.Handler {
 	r.GET("/v1/user/profile", h.middleware.FirebaseAuthMiddleware(middleware.InitFirebaseClient()), h.handleGetUser)
 	r.PUT("/v1/user/profile", h.middleware.FirebaseAuthMiddleware(middleware.InitFirebaseClient()), h.handleUpdateUser)
 	r.GET("/v1/users/profiles", h.middleware.FirebaseAuthMiddleware(middleware.InitFirebaseClient()), h.handleGetAllUsers)
+	r.POST("v1/match", h.middleware.FirebaseAuthMiddleware(middleware.InitFirebaseClient()), h.handleCalculateMatch)
 	// r.POST("/message", h.handleCreateMessage)
 	// r.GET("/message/:id", h.handleGetMessage)
 	// r.DELETE("/message/:id", h.handleDeleteMessage)
