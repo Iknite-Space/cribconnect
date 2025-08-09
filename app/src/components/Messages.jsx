951bfd05e9a01e533fa3e92babcc3a383f98babe
AMPAM-Estella <estellaabo@gmail.com>
2025-08-09 18:44:18 +0100

    Separated the message component from the MessagePage

--- app/src/components/Messages.jsx
+++ app/src/components/Messages.jsx
@@ -1,52 +1,25 @@
 import useMessages from "../hooks/useMessages";
+import { useState } from "react";
 import "./Messages.css";
 
-const Messages = ({ selectedUser }) => {
-  const user = [
-    { id: 1, name: "Bob" },
-    { id: 2, name: "Allen" },
-    { id: 3, name: "Glenn" }
-  ];
-  const { messages, isUnlocked, isLoading, error } = useMessages(
-    selectedUser?.id
-  );
+const Messages = ({ threadId }) => {
+  const { messages, isLocked, isLoading, error } = useMessages(threadId);
 
-  // if (isLoading) return <div>Loading messages...</div>;
-  // if (error) return <div>Error loading messages: {error.message}</div>;
-  // if (!isUnlocked) return <div>This chat is locked. Please pay first.</div>;
+  if (isLoading) return <div>Loading messages...</div>;
+  if (error && error.code !== 404)
+    return <div>Error loading messages: {error.message}</div>;
+  if (messages && messages.length === 0) return <div>No messages.</div>;
+  if (messages && isLocked)
+    return <div>This chat is locked. Please pay first.</div>;
   return (
     <>
-      <div className='sidebar'>
-        <h2>Chats</h2>
-        <ul>
-          
-          {user.map((u) => (
-            <li key={u.id}>{u.name}</li>
-          ))}
-        </ul>
+      <div className='chat-window'>
         {(messages || []).map((msg, index) => (
           <div key={index} className='message'>
             <strong>{msg.sender}: </strong>
             {msg.text}
           </div>
         ))}
-      </div>
-      <div className='chat-window'>
-        {selectedUser ? (
-          <>
-            <div className='chat-header'>Chat with {selectedUser.name}</div>
-            <div className='chat-box'>
-              {(messages[selectedUser.id] || []).map((msg, index) => (
-                <div key={index} className='chat-message'>
-                  <strong>{msg.sender}: </strong>
-                  {msg.text}
-                </div>
-              ))}
-            </div>
-          </>
-        ) : (
-          <div className='no-chat'>Select a user to start chatting</div>
-        )}
       </div>
     </>
   );
